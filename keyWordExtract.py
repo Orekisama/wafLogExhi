c5aa2bc1470b5c380b51cf89acaf0cc4c15aa476
Orekisama <michael2cindy@163.com>
2018-05-31 14:23:37 +0800

    Orekisama
    rename variables

--- keyWordExtract.py
+++ keyWordExtract.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 with open('waf_audit.log', 'r') as f1, open('keyWordAll.txt', 'w') as f2:
-    for line in f1.readlines():
-        if '---' in line:
+    for keyword in f1.readlines():
+        if '---' in keyword:
             f2.write(line.strip('\n').strip('-') + '\n')
